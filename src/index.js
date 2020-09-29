5ef87da9827cec47c48ad07837290729ba9d493b
luistapls <llopezampiez7@gmail.com>
2020-09-28 21:26:58 -0430

    first commit

---
+++ src/index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.set('port', process.env.PORT || 3000);
+
+app.use(require('./routes/api'));
+
+app.listen(app.get('port'), () => {
+  console.log('Server on port: ', app.get('port'));
+});
